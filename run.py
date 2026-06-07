9072327d00cbd6f37163611fd6a8f82d997a318a
dhruvjoker <134125943+dhruvjoker@users.noreply.github.com>
2026-06-07 23:14:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -11,9 +11,9 @@ Production (Render / Railway / Heroku / VPS):
 import os
 from app import create_app, socketio
 
+# Change this line in run.py
 env  = os.environ.get('FLASK_ENV', 'development')
 app  = create_app(env)
-
 if __name__ == '__main__':
     debug = env == 'development'
     socketio.run(app, debug=debug, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
